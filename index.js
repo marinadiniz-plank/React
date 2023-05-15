5481861b5c293fb46d4b73c6144adaab427b16ec
marinadiniz-plank <marina.diniz@joinplank.com>
2023-05-15 15:29:14 -0300

    starting react

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const navbar = (
+    <nav>
+        <h1>Abacaxi</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
